abd9193a07f8c545df6832066bf2be5a836411af
Bhavesh Saini <1bhavesh.saini1@gmail.com>
2020-03-24 13:30:19 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,8 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 app.use(cors())
-// const corsOptions ={
-// 	origin: 'http://localhost:3000',
-// 	methods: ['POST', 'GET']
-// }
-// app.use(cors(corsOptions))
-
-// app.use((req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
 
 
 app.use(express.static('profilePictures'));
@@ -23,5 +13,14 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-app.listen(80)
+// if(process.env.NODE_ENV === 'production'){
+// 	app.use(express.static('../../task-manager-ui/build'))
+
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.join(__dirname, '../../task-manager-ui', 'build', 'index.html'))
+// 	})
+// }
+
+const PORT = process.env.PORT || 80
+app.listen(PORT)
 
